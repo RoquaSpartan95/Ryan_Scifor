e04aee4cc0aacc6cdfd09ebf0ac25604776ce004
RoquaSpartan95 <139156203+RoquaSpartan95@users.noreply.github.com>
2023-12-19 11:53:33 +0530

    Delete Ryan1.py

--- Ryan1.py
+++
@@ -1,9 +0,0 @@
-myList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print("The given list is:")
-print(myList)
-list_len=len(myList)
-sum=0
-for i in range(list_len):
-    sum=sum+myList[i]
-
-print("Sum of all the elements in the list is:", sum)
