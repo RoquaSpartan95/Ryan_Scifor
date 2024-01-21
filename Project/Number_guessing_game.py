3dbdaed576e6e528e617b3cfa0f2430e91bf4292
RoquaSpartan95 <139156203+RoquaSpartan95@users.noreply.github.com>
2024-01-21 18:12:00 +0530

    Add files via upload

---
+++ Project/Number_guessing_game.py
@@ -0,0 +1,51 @@
+import random
+import os
+
+logo = """
+  _   __                 _                    _____                     
+ | \ |  |               | |                  / ____|                    
+ |  \|  |_   _ _ __ ___ | |__   ___  __ __  | |  __ _   __  ____   ____   ____ 
+ | . `  | | | | '_ ` _ \| '_ \ / _ \| ' __| | | |_ | | |  |/  _ \ /      / 
+ | |\   | |_| | | | | | | |_) /| __/| / |   | |__| | |_|  |   __/ \____  \____   
+ | | \  |\  | | | | | | | | /  \    | '/    \      |\     |\    |      |      |
+ |_|  \_| \_|_| |_| |_| .__/    \__/\_/      \_____| \__,_| \___| \___/  \___/
+"""
+os.system("clear")
+print(logo)
+
+
+def difficult(easy_or_hard):
+    if easy_or_hard == "easy":
+        return 10
+    elif easy_or_hard == "hard":
+        return 5
+    else:
+        again = input("Invalid syntax. Please enter 'easy' or 'hard': ")
+        return difficult(again)
+
+
+chosen_number = random.randint(1, 100)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+attempts = difficult(difficulty)
+print(f"You have {attempts} attempts remaining to guess the number.")
+guess=0
+while guess!=chosen_number:
+    guess = int(input("Make a guess: "))
+    attempts -= 1
+    if attempts == 0:
+        print(f"You've run out of guesses, you lose! The answer was {chosen_number}.")
+        break
+    if guess > chosen_number:
+        print("Too high.")
+    elif guess < chosen_number:
+        print("Too low.")
+    elif guess == chosen_number:
+        print(f"You got it!. The answer was {chosen_number}.")
+        break
+    else:
+        print(f"You lose the game!. The answer was {chosen_number}.")
+        break
+    print("Guess again.")
+    print(f"You have {attempts} attempts remaining to guess the number.")
